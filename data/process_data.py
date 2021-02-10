d4d685da4fb8635a7b500a6bf36c51c26a7c251b
alex.pereira <avqpereira@gmail.com>
2021-02-10 19:57:49 -0300

    Process: Indentation & Cleaning enhanced

--- data/process_data.py
+++ data/process_data.py
@@ -63,6 +63,9 @@ def clean_data(df):
   # drop duplicates
   df.drop_duplicates(inplace = True)
 
+  # slicing out where related = 2
+  df = df.loc[df.related != 2]
+
   return df
 
 
@@ -113,31 +116,32 @@ def main():
   Output:
     None
   """
-    if len(sys.argv) == 4:
-      messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-      print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-        .format(messages_filepath, categories_filepath))
-      df = load_data(messages_filepath, categories_filepath)
+  if len(sys.argv) == 4:
+    messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-      print('Cleaning data...')
-      df = clean_data(df)
+    print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+      .format(messages_filepath, categories_filepath))
+    df = load_data(messages_filepath, categories_filepath)
 
-      print('Attempting to drop table if it exists')
-      drop_table('MessagesCategories', database_filepath)
+    print('Cleaning data...')
+    df = clean_data(df)
+
+    print('Attempting to drop table if it exists')
+    drop_table('MessagesCategories', database_filepath)
+      
+    print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+    save_data(df, database_filepath)
         
-      print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-      save_data(df, database_filepath)
-        
-      print('Cleaned data saved to database!')
+    print('Cleaned data saved to database!')
     
-    else:
-      print('Please provide the filepaths of the messages and categories '\
-            'datasets as the first and second argument respectively, as '\
-            'well as the filepath of the database to save the cleaned data '\
-            'to as the third argument. \n\nExample: python process_data.py '\
-            'disaster_messages.csv disaster_categories.csv '\
-            'DisasterResponse.db')
+  else:
+    print('Please provide the filepaths of the messages and categories '\
+          'datasets as the first and second argument respectively, as '\
+          'well as the filepath of the database to save the cleaned data '\
+          'to as the third argument. \n\nExample: python process_data.py '\
+          'disaster_messages.csv disaster_categories.csv '\
+          'DisasterResponse.db')
 
 
 if __name__ == '__main__':
